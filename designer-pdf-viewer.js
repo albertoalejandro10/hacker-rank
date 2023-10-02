d07633e64f35e921359669b323590cc274a4b392
AlbertoAlejandro <alberto196g@gmail.com>
2023-10-02 18:52:02 -0400

    designer-pdf-viewer test

---
+++ designer-pdf-viewer.js
@@ -0,0 +1,34 @@
+/*
+ * Complete the 'designerPdfViewer' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY h
+ *  2. STRING word
+ */
+
+function designerPdfViewer (h, word) {
+  const letters = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  const arr = h.map((element, index) => {
+    return {
+      letter: letters[index],
+      number: element
+    }
+  })
+  // console.log(arr)
+
+  const words = word.split('')
+  const result = words.map(element => {
+    return arr.filter(x => x.letter === element)
+  })
+  const numbers = result.map(function (obj) {
+    return obj[0].number
+  })
+
+  const maxNumber = Math.max.apply(Math, numbers)
+  return maxNumber * word.length
+}
+
+const h = [1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7]
+const word = 'zaba'
+console.log(designerPdfViewer(h, word))
